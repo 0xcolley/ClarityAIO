ea46c97e7f6e3e1f6e9fb69ddab12a5204e9bd6f
0xcolley <colletterc@vcu.edu>
2023-12-21 00:12:56 -0500

    added static compute on buy/sell, will update to dynamic/auto

---
+++ src/modules/raydium/ray_compute.ts
@@ -0,0 +1,47 @@
+import { Connection, PublicKey } from "@solana/web3.js";
+import { Liquidity, Percent, Token, TokenAmount,CurrencyAmount, LiquidityPoolInfo } from "@raydium-io/raydium-sdk";
+
+export async function live_compute (connection: Connection, poolKeys: any, curr_in: PublicKey, curr_out: PublicKey, amount_in: number, slip: number) {
+    try {
+        const poolInfo = await Liquidity.fetchInfo({connection, poolKeys});
+
+        var in_decimal: number;
+        var out_decimal: number;
+
+        if (curr_in.toBase58() === poolKeys.baseMint.toBase58()) {
+            in_decimal = poolInfo.baseDecimals
+            out_decimal = poolInfo.quoteDecimals
+        } 
+        else {
+            out_decimal = poolInfo.baseDecimals;
+            in_decimal = poolInfo.quoteDecimals;
+        }
+
+        const amountIn = new TokenAmount(new Token(curr_in, in_decimal), amount_in, false);
+        const currencyOut = new Token(curr_out, out_decimal);
+        const slippage = new Percent(slip, 100)
+      
+        const {
+            amountOut,
+            minAmountOut,
+            currentPrice,
+            executionPrice,
+            priceImpact,
+            fee,
+        } = Liquidity.computeAmountOut({ poolKeys, poolInfo, amountIn, currencyOut, slippage})
+        
+        return [
+            amountOut,
+            minAmountOut,
+            currentPrice,
+            executionPrice,
+            priceImpact,
+            fee,
+            amountIn,
+        ]
+    }
+    catch(e) {
+        console.log(e);
+        return 1;
+    }
+}
